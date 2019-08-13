b6ea52c0a6a97c64a2b880e90334253135a182f3
Student <example@example.com>
2019-08-13 15:40:47 -0700

    will add divs, creates flashcards,adds buttons, hides paragraphs

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  $("button#green").click(function(){
+    $(".greenish").toggle();
+  });
+  $("button#yellow").click(function(){
+    $(".yellowish").toggle();
+  });
+  $("button#red").click(function(){
+    $(".redish").toggle();
+  });
+  $("button#blue").click(function(){
+    $(".blueish").toggle();
+  });
+
+
+
+//   //light switch button function
+//   $("button#blue").click(function() {
+//     $("body").removeClass();
+//     $("body").toggleClass("blue-background");
+//   });
+//
+//
+// //methods for colored buttons
+//   $("button#green").click(function() {
+//     $("body").removeClass();
+//     $("body").toggleClass("green-background");
+//   });
+//   $("button#yellow").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("yellow-background");
+//   });
+//   $("button#red").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("red-background");
+//   });
+//
+//   //highlight function (makeItAbox)
+//   $("p#highlight").click(function() {
+//     $("p#highlight").toggleClass("makeItAbox");
+//   });
+});
